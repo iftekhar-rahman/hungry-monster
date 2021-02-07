fdd4e587245328cde83f2eacaa10712b746a2ce6
Iftekhar Rahman <iftekharmail1@gmail.com>
2021-02-07 21:44:07 +0600

    meal item added

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
-// const foodName = displayMeals(data.meals.strMeal);
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
-.then(res => res.json())
-.then(data => displayMeals(data.meals));
 
+/*----------------- Meal item list ---------------------*/ 
+const containerDiv = document.getElementById('meals');
+const searchBtn = document.getElementById('submit');
 
-fetch('https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview')
-.then(res => res.json())
-.then(data => displayMeals(data.meals));
+searchBtn.addEventListener('click', mealList => {
+    const mealInput = document.getElementById('meal-input').value.trim();
 
-
-
-const searchBtn = document.getElementById('submit'); 
-const inputValue = document.getElementById('meal-input').value;
-searchBtn = document.addEventListener('click', function(foodName){
-    // const mealInput = document.getElementById('meal-input').value;
-    inputValue = foodName;
-    return foodName;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals));
 });
 
-
 const displayMeals = mealBox => {
-    const containerDiv = document.getElementById('meals');
-
-    // meals.forEach(meal => {
-
-    //     const singleDiv = document.createElement('div');
-
-    //     const mealsDetails = `
-    //         <img src="${meal.strMealThumb}">
-    //         <h4>${meal.strMeal}</h4>
-    //     `;
-    //     mealsDetails.document.innerHTML = mealsDetails;
-    //     containerDiv.appendChild(singleDiv);
-    // });
-
-
-    for (let i = 0; i < mealBox.length; i++) {
-        const meal = mealBox[i];
-
-        const singleDiv = document.createElement('div');
-        singleDiv.className = 'single-meal';
-        const mealsDetails = `
-            <img src="${meal.strMealThumb}">
-            <h4>${meal.strMeal}</h4>
-        `;
-        singleDiv.innerHTML = mealsDetails;
-        containerDiv.appendChild(singleDiv);
-        
+    if(mealBox){
+        let mealsDetails = '';
+        mealBox.forEach(meal => {
+            mealsDetails += `
+                <div class="single-meal" data-id="${meal.idMeal}">
+                    <img src="${meal.strMealThumb}">
+                    <h4>${meal.strMeal}</h4>
+                </div>
+            `;
+            containerDiv.innerHTML = mealsDetails;
+        });
+    } else{
+        alert("Sorry, we didn't find anything.")
     }
+    
 }
 
+// /*--------  Meal Details -----------*/ 
+// const mealContainer = document.getElementById('meals');
+// // const recipeClose = document.getElementById('close');
+// mealContainer.addEventListener('click', function(event){
+//     event.target.classList.add('single-meal');
+
+// }); 
+
+// // let mealItems = document.getElementsByClassName('single-meal');
+// // for (let i = 0; i < mealItems.length; i++) {
+// //     let mealItem = mealItems[i];
+// //     mealItem.onclick = function(){
+// //         this.classList.add('active');
+// //     }
+    
+// }
